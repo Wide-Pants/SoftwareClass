89e3895829c345eb27e39697f0540bd7b4736293
YunHo Kang <dbsgh123030@gmail.com>
2024-06-12 17:55:17 +0900

    feat searchBook_bs

--- SoftWareClass/src/BookList.java
+++ SoftWareClass/src/BookList.java
@@ -12,12 +12,12 @@ public class BookList {
 				throw new Exception("중복 발견");
 			}
 		}
-		//정렬된 위치에 삽입 추후 이진트리를 위해 미리 정렬
+		// 정렬된 위치에 삽입 추후 이진트리를 위해 미리 정렬
 		int index = 0;
-        while (index < bookList.size() && bookList.get(index).id < newBook.id) {
-            index++;
-        }
-        bookList.add(index, newBook); 
+		while (index < bookList.size() && bookList.get(index).id < newBook.id) {
+			index++;
+		}
+		bookList.add(index, newBook);
 		System.out.println(String.format("Book {id: '%d', 제목: '%s', 저자: '%s', 출판년도: %d}를 추가하였습니다.", newBook.id,
 				newBook.title, newBook.author, newBook.pubYear));
 		return;
@@ -39,7 +39,7 @@ public class BookList {
 	public void searchBook(int id) throws Exception {
 		for (Book book : bookList) {
 			if (book.id == id) {
-				System.out.println(String.format("{id: %d, 제목: %s, 저자: %s, 연도: %d}를 찾았습니다.", id, book.title,
+				System.out.println(String.format("Book {id: %d, 제목: %s, 저자: %s, 연도: %d}를 찾았습니다.", id, book.title,
 						book.author, book.pubYear));
 				return;
 			}
@@ -53,12 +53,38 @@ public class BookList {
 		for (Book book : bookList) {
 			if (book.id == id) {
 				bookList.remove(book);
-				System.out.println(String.format("{id: %d, 제목: %s, 저자: %s, 연도: %d}를 삭제했습니다.", id, book.title,
+				System.out.println(String.format("Book {id: %d, 제목: %s, 저자: %s, 연도: %d}를 삭제했습니다.", id, book.title,
 						book.author, book.pubYear));
 				return;
 			}
 		}
 		// arraylist 에 존재하지 않으면 예외처리
+		System.out.println(String.format("해당 ID(%d)의 도서를 찾을 수 없습니다.", id));
+		throw new Exception("데이터 없음");
+	}
+
+	public void searchBook_bs(int id) throws Exception {
+		int leftId = 0;
+		int rightId = bookList.size() - 1;
+
+		while (leftId <= rightId) {
+			int mid = leftId + (rightId - leftId) / 2;
+			Book midBook = bookList.get(mid);
+
+			if (midBook.id == id) {
+				System.out.println(String.format("Book {id: %d, 제목: %s, 저자: %s, 연도: %d}를 찾았습니다.", id, midBook.title,
+						midBook.author, midBook.pubYear));
+				return;
+			}
+
+			if (midBook.id < id) {
+				leftId = mid + 1;
+			} else {
+				rightId = mid - 1;
+			}
+		}
+
+		// arraylist에 존재하지 않으면 예외처리
 		System.out.println(String.format("해당 ID(%d)의 도서를 찾을 수 없습니다.", id));
 		throw new Exception("데이터 없음");
 	}
