d75798fd45373d0c099d412a86f1665685867c5b
YunHo Kang <dbsgh123030@gmail.com>
2024-06-16 22:10:37 +0900

    refactor book class

--- SoftWareClass/src/BookList.java
+++ SoftWareClass/src/BookList.java
@@ -2,15 +2,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class BookList {
-
-	//빌드 테스트하려고 주석 추가했다!22
 	
 	private List<Book> bookList = new ArrayList<>();
+
 	public class Book {
-		int id;// 아이디
-		String title;// 제목
-		String author;// 저자
-		int pubYear;// 발행연도
+		private int id;// 아이디
+		private String title;// 제목
+		private String author;// 저자
+		private int pubYear;// 발행연도ㄴ
 		
 		public Book(int id, String title, String author, int pubYear) {
 	        this.id = id;
@@ -18,23 +17,42 @@ public class BookList {
 	        this.author = author;
 	        this.pubYear = pubYear;
 	    }
+		public Book(String title, String author, int pubYear) {
+	        this.id = bookList.size();
+	        this.title = title;
+	        this.author = author;
+	        this.pubYear = pubYear;
+	    }
+
+		public int getId(){
+			return this.id;
+		}
+		public String getTitle(){
+			return this.title;
+		}
+		public String getAuthor(){
+			return this.author;
+		}
+		public int getPubYear(){
+			return this.pubYear;
+		}
 	}
 	
 	public void addBook(Book newBook) throws Exception {
 		for (Book book : bookList) {
-			if (book.id == newBook.id) {// 중복 ID 값에 대해 예외처리
+			if (book.getId() == newBook.getId()) {// 중복 ID 값에 대해 예외처리
 				System.out.println(String.format("해당 ID(%d)는 이미 존재합니다.", newBook.id));
 				throw new Exception("중복 발견");
 			}
 		}
 		// 정렬된 위치에 삽입 추후 이진트리를 위해 미리 정렬
 		int index = 0;
-		while (index < bookList.size() && bookList.get(index).id < newBook.id) {
+		while (index < bookList.size() && bookList.get(index).getId() < newBook.getId()) {
 			index++;
 		}
 		bookList.add(index, newBook);
-		System.out.println(String.format("Book {id: '%d', 제목: '%s', 저자: '%s', 출판년도: %d}를 추가하였습니다.", newBook.id,
-				newBook.title, newBook.author, newBook.pubYear));
+		System.out.println(String.format("Book {id: '%d', 제목: '%s', 저자: '%s', 출판년도: %d}를 추가하였습니다.", newBook.getId(),
+				newBook.getTitle(), newBook.getAuthor(), newBook.getPubYear()));
 		return;
 
 	}
@@ -46,16 +64,16 @@ public class BookList {
 		}
 		System.out.print("전체 조회:\n");
 		for (Book book : bookList) {
-			System.out.println(String.format("Book {id: '%d', 제목: '%s', 저자: '%s', 출판년도: %d}", book.id, book.title,
-					book.author, book.pubYear));
+			System.out.println(String.format("Book {id: '%d', 제목: '%s', 저자: '%s', 출판년도: %d}", book.getId(),
+				book.getTitle(), book.getAuthor(), book.getPubYear()));
 		}
 	}
 
 	public void searchBook(int id) throws Exception {
 		for (Book book : bookList) {
-			if (book.id == id) {
-				System.out.println(String.format("Book {id: %d, 제목: %s, 저자: %s, 연도: %d}를 찾았습니다.", id, book.title,
-						book.author, book.pubYear));
+			if (book.getId() == id) {
+				System.out.println(String.format("Book {id: %d, 제목: %s, 저자: %s, 연도: %d}를 찾았습니다.", id,
+				book.getTitle(), book.getAuthor(), book.getPubYear()));
 				return;
 			}
 		}
@@ -66,10 +84,10 @@ public class BookList {
 
 	public void deleteBook(int id) throws Exception {
 		for (Book book : bookList) {
-			if (book.id == id) {
+			if (book.getId() == id) {
 				bookList.remove(book);
-				System.out.println(String.format("Book {id: %d, 제목: %s, 저자: %s, 연도: %d}를 삭제했습니다.", id, book.title,
-						book.author, book.pubYear));
+				System.out.println(String.format("Book {id: %d, 제목: %s, 저자: %s, 연도: %d}를 삭제했습니다.", id,
+				book.getTitle(), book.getAuthor(), book.getPubYear()));
 				return;
 			}
 		}
@@ -86,13 +104,13 @@ public class BookList {
 			int mid = leftId + (rightId - leftId) / 2;
 			Book midBook = bookList.get(mid);
 
-			if (midBook.id == id) {
-				System.out.println(String.format("Book {id: %d, 제목: %s, 저자: %s, 연도: %d}를 찾았습니다.", id, midBook.title,
-						midBook.author, midBook.pubYear));
+			if (midBook.getId() == id) {
+				System.out.println(String.format("Book {id: %d, 제목: %s, 저자: %s, 연도: %d}를 찾았습니다.", id, midBook.getTitle(),
+						midBook.getAuthor(), midBook.getPubYear()));
 				return;
 			}
 
-			if (midBook.id < id) {
+			if (midBook.getId() < id) {
 				leftId = mid + 1;
 			} else {
 				rightId = mid - 1;
